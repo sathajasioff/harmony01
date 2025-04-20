7858b5ca759903d8c052a146e54600ca897728e6
LavanyaM619 <lalumogan619@gmail.com>
2025-04-20 15:07:19 +0530

    ll

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Middleware
 app.use(cors());
-app.use(express.json.stringify({ limit: '1mb' }));
+app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ limit: '1mb', extended: true }));
 
 // MongoDB Connection
